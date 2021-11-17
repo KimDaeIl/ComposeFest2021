5a846c69fd743887b8587ade8e2a22e5cecc03c4
KimDaeil <qatta24@gmail.com>
2021-11-17 12:51:44 +0900

    week 1 chapter 12

--- week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/codelab/basics/ui/theme/Color.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/codelab/basics/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Navy = Color(0xFF073042)
+val Blue = Color(0xff4285f4)
+val LightBlue = Color(0xffd7effe)
+val Chartreuse = Color(0xfeeff7cf)
