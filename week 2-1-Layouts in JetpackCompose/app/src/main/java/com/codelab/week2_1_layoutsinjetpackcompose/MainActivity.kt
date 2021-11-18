05699260795b69bdf912f6dc4286e77753fa0acc
KimDaeil <qatta24@gmail.com>
2021-11-18 13:53:06 +0900

    week 2 chapter 3

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/codelab/week2_1_layoutsinjetpackcompose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/codelab/week2_1_layoutsinjetpackcompose/MainActivity.kt
@@ -3,11 +3,23 @@ package com.codelab.week2_1_layoutsinjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelab.week2_1_layoutsinjetpackcompose.ui.theme.Week21LayoutsInJetpackComposeTheme
 
 class MainActivity: ComponentActivity() {
@@ -16,10 +28,7 @@ class MainActivity: ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Week21LayoutsInJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+              PhotographerCard()
             }
         }
     }
@@ -30,10 +39,49 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    /*
+    Row 의 clickable, padding 의 순서에 따라 클릭 이벤트 영역이 달라진다.
+    clickable을 지정하는 시점의 영역이 이벤트 대상이 된다.
+
+    padding -> clickable >> padding이 적용이 된 부분만 이벤트 영역
+    clickable -> padding >> clickable 영역에 padding을 설정해서 전체 영역이 클릭 이벤트 영역
+
+     */
+    Row(
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable { }
+            .padding(16.dp)
+    ) {
+
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes agoe", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Week21LayoutsInJetpackComposeTheme {
-        Greeting("Android")
+        PhotographerCard()
     }
 }
